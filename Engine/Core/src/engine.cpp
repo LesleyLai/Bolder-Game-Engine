759984d98f25711260ed3936e5c417beaae99131
Lesley Lai <laisililai@live.cn>
2017-07-30 12:25:25 +0800

    Start on game loop

--- Engine/Core/src/engine.cpp
+++ Engine/Core/src/engine.cpp
@@ -1,7 +1,21 @@
+#include <chrono>
+
 #include "bolder/exception.hpp"
 #include "bolder/engine.hpp"
 #include "bolder/display.hpp"
+#include "bolder/logger.hpp"
 #include "bolder/opengl_context.hpp"
+
+namespace {
+    // Check if fps is too low and report to logger
+    void check_fps_too_lower(int fps) {
+        constexpr int fps_min = 30;
+        if (fps < fps_min) {
+            BOLDER_LOG_WARNING << "Low frame rate: " << fps << " fps";
+            // Todo: Report snapshot of current status of game
+        }
+    }
+}
 
 namespace bolder {
 namespace detail {
@@ -15,14 +29,51 @@ struct Engine_impl {
 
     }
 
-    int exec(int /*argc*/, char** /*argv*/) {
-        while (!display.closed()) {
-            gl_context.render();
-            display.update();
-        }
-        return 0;
-    }
+    int exec(int argc, char** argv);
+
+    void game_loop();
 };
+
+int Engine_impl::exec(int, char**) {
+    game_loop();
+    return 0;
+}
+
+void Engine_impl::game_loop()
+{
+    using namespace std::chrono;
+    auto previous = high_resolution_clock::now();
+    duration<double, std::milli> lag {0};
+
+    // Time s(ms) between two update
+    constexpr milliseconds ms_per_update {10};
+
+    while (!display.closed()) {
+        auto current = high_resolution_clock::now();
+        const auto delta_time = current - previous;
+        int fps = static_cast<int>(1s / delta_time);
+
+        previous = current;
+        lag += delta_time;
+
+        // Todo: process input
+
+        while (lag >= ms_per_update) {
+            // Todo: None graphics update
+            lag -= ms_per_update;
+            // Todo: report error if update takes longer that ms_per_update
+        }
+
+        // Render
+        gl_context.render();
+
+        // Swap buffer and deal platform message
+        display.update();
+
+        check_fps_too_lower(fps);
+        // Todo: Set upper limit of the frame rate
+    }
+}
 
 }
 
